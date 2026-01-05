b7e30c437e35da866db1c1d2879d459b53d0bdd3
juanhb9 <juanhb9@gmail.com>
2026-01-04 21:29:03 -0600

    fallos al inicio

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const sobre = document.getElementById("sobre");
 
-  sobre.addEventListener("click", () => {
-    // Animación antes de redirigir
-    sobre.classList.add("animar-sobre");
+  if (sobre) {
+    sobre.addEventListener("click", () => {
+      sobre.classList.add("animar-sobre");
 
-    setTimeout(() => {
-      window.location.href = "invitacion.html"; // redirige a la otra página
-    }, 1500);
-  });
+      setTimeout(() => {
+        window.location.href = "./invitacion.html";
+      }, 1500);
+    });
+  } else {
+    console.error("No se encontró el elemento con id='sobre'");
+  }
 });
